dbf22442fdb3291d9d1f6df3ffb4dc94e48d9522
Khoa Phan <khoa.anh.phan162@gmail.com>
2022-08-05 19:42:31 -0700

    Basic MUI setup

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import BasicTextFields from '../components/TextField'
 
 const ProfilePage = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <BasicTextFields/>
+    </>
+
   )
 }
 

ca91066457b50e8f5eff6accbcbbcdf05a7ba64d
Khoa Phan <khoa.anh.phan162@gmail.com>
2022-08-23 13:50:25 -0700

    Name and State labels added

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
+const states = [
+  {
+    value: 'California'
+  },
+  {
+    value: 'California'
+  },
+  {
+    value: 'California'
+  },
+  {
+    value: 'Oregon'
+  },
+];
 
 export default function BasicTextFields() {
+
+  const [state, setState] = React.useState('Oregon');
+  const handleStateChange = (event) => {
+    setState(event.target.value);
+  };
+
   return (
     <Box
       component="form"
@@ -12,9 +35,29 @@ export default function BasicTextFields() {
       noValidate
       autoComplete="off"
     >
-      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-      <TextField id="filled-basic" label="Filled" variant="filled" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <TextField
+        id="standard-name-input"
+        label="Name"
+        type="name"
+        variant="standard"
+      />
+
+      <TextField
+        id="standard-states-select"
+        select
+        label="State"
+        value={state}
+        onChange={handleStateChange}
+        helperText="Please select your state"
+        variant="standard"
+      >
+        {states.map((option) => (
+          <MenuItem key={option.value} value={option.value}>
+            {option.value}
+          </MenuItem>
+        ))}
+      </TextField>
+
     </Box>
   );
 }

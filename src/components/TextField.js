91c10057be3514963d55a8514c53cffe1d548fe0
Khoa Phan <khoa.anh.phan162@gmail.com>
2022-08-23 14:00:16 -0700

    States added for dropdown

--- src/components/TextField.js
+++ src/components/TextField.js
@@ -5,23 +5,36 @@ import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 
 const states = [
-  {
-    value: 'California'
-  },
-  {
-    value: 'California'
-  },
-  {
-    value: 'California'
-  },
-  {
-    value: 'Oregon'
-  },
+  {value: 'Alabama'}, {value: 'Alaska'},
+  {value: 'Arizona'}, {value: 'Arkansas'},
+  {value: 'California'}, {value: 'Colorado'},
+  {value: 'Delaware'}, {value: 'Florida'},
+  {value: 'Georgia'}, {value: 'Hawaii'},
+  {value: 'Idaho'}, {value: 'Illinois'},
+  {value: 'Indiana'}, {value: 'Iowa'},
+  {value: 'Kansas'}, {value: 'Kentucky'},
+  {value: 'Louisiana'}, {value: 'Maine'},
+  {value: 'Maryland'}, {value: 'Massachusetts'},
+  {value: 'Michigan'}, {value: 'Minnesota'},
+  {value: 'Mississippi'}, {value: 'Missouri'},
+  {value: 'Montana'}, {value: 'Nebraska'},
+  {value: 'Nevada'}, {value: 'New Hampshire'},
+  {value: 'New Jersey'}, {value: 'New Mexico'},
+  {value: 'New York'}, {value: 'North Carolina'},
+  {value: 'North Dakota'}, {value: 'Ohio'},
+  {value: 'Oklahoma'}, {value: 'Oregon'},
+  {value: 'Pennsylvania'}, {value: 'Rhode Island'},
+  {value: 'South Carolina'}, {value: 'South Dakota'},
+  {value: 'Tennessee'}, {value: 'Texas'},
+  {value: 'Utah'}, {value: 'Vermont'},
+  {value: 'Virginia'}, {value: 'Washington'},
+  {value: 'West Virginia'}, {value: 'Wisconsin'}, 
+  {value: 'Wyoming'}
 ];
 
 export default function BasicTextFields() {
 
-  const [state, setState] = React.useState('Oregon');
+  const [state, setState] = React.useState('');
   const handleStateChange = (event) => {
     setState(event.target.value);
   };

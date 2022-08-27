59b79be55c88d27f416ff1025b1bcbe0223dc8b6
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2022-08-26 18:26:17 -0700

    Revert "Merge remote-tracking branch 'origin/khoa-2' into jalen"
    This reverts commit bb121fb3111da8384e92c26782d9f66afa5bc035, reversing
    changes made to 6fc91723bc92a29f9b77f1d0f6e0be7e3daa1613.

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,22 +1,18 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import TemporaryDrawer from '../components/ProfileDrawer'
-import BasicTextFields from '../components/TextField'
 
 const ProfilePage = () => {
   return (
     <>
-      <div id = "button2">
-      <Button 
-      variant="contained">{TemporaryDrawer()}
-      </Button>
-      </div>
-      <div>
-        I love Bandwagons
-      </div>
-      <>
-      <BasicTextFields/>
-      </>
+        <div id = "button2">
+        <Button 
+        variant="contained">{TemporaryDrawer()}
+        </Button>
+        </div>
+        <div>
+          I love Bandwagons
+        </div>
     </>
 
   )

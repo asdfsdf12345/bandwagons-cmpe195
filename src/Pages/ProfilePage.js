5147af0f1a7f77160f83c7f573710885cb410110
Khoa Phan <khoa.anh.phan162@gmail.com>
2022-08-26 17:23:47 -0700

    Added UI changes from khoa branch, without Firebase

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import TemporaryDrawer from '../components/ProfileDrawer'
+import BasicTextFields from '../components/TextField'
 
 const ProfilePage = () => {
   return (
     <>
-        <div id = "button2">
-        <Button 
-        variant="contained">{TemporaryDrawer()}
-        </Button>
-        </div>
-        <div>
-          I love Bandwagons
-        </div>
+      <div id = "button2">
+      <Button 
+      variant="contained">{TemporaryDrawer()}
+      </Button>
+      </div>
+      <div>
+        I love Bandwagons
+      </div>
+      <>
+      <BasicTextFields/>
+      </>
     </>
 
   )

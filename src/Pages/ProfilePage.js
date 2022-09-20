832e06c0131cc57491602bffb5b1a417dba5a84c
Khoa Phan <khoa.anh.phan162@gmail.com>
2022-09-20 15:53:36 -0700

    Profile avatar button worked on

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useState } from "react";
 import Button from '@material-ui/core/Button'
 import TemporaryDrawer from '../components/ProfileDrawer'
 import BasicTextFields from '../components/TextField'
 import HobbyTagChoices from '../components/HobbyTag'
 import ProfileCreationButton from '../components/ProfileCreation'
+import IconButton from '@mui/material/IconButton';
+import Avatar from "@material-ui/core/Avatar";
 
 const ProfilePage = () => {
+  
   return (
     <>
       <div id = "button2">
@@ -14,7 +18,10 @@ const ProfilePage = () => {
         </Button>
       </div>
       <div>
-        I love Bandwagons
+        <IconButton color="primary" aria-label="upload picture" component="label">
+          <input hidden accept="image/*" type="file" />
+          <Avatar alt="profile-avatar" style={{ height: '80px', width: '80px' }}></Avatar>
+        </IconButton>
       </div>
       <><BasicTextFields/></>
       <div>Pick your preferences</div>

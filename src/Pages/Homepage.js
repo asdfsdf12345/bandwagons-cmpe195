3bdf280fe6f58d85f642f7a4d175212fb7c265de
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2022-12-02 23:10:39 -0800

    done done

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,19 +1,70 @@
 import React from 'react'
 import Banner from '../components/Banner'
-import Description from '../components/Description'
-import FriendFind from '../components/FriendFind'
-import GroupFind from '../components/GroupFind'
+import MakePost from '../components/MakePost';
+import Post from '../components/Post'
+import { NavigationState } from "../NavigationContext";
+import { doc, collection, Timestamp, addDoc, Firestore, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { useState } from 'react';
+import { Grid } from "@material-ui/core";
+import GroupFind from '../components/GroupFind';
+import FriendFind from '../components/FriendFind';
+import Description from '../components/Description';
 
 const Homepage = () => {
-  return (
     <>
     <GroupFind></GroupFind>
-    <FriendFind></FriendFind>
-    <Banner/>
-    <Description/>
-    </>
 
-  )
+  const {user, setAlert} = NavigationState();
+  const {postID, setPostID} = useState("3EY641Z4FSYMwAzDnFsNVDw0IaO21670004194")
+  const [postQueueArray, setPostQueueArray] = useState([]);
+
+  React.useEffect(async () => {
+    if (user) {
+      //MULTIPLE DATA QUERY BLOCK aka GET DATA
+      const q = query(collection(db, "Posts"), where("creatorEmail", "==", "abc@def.com"));
+      const querySnapshot = await getDocs(q);
+      const queryResult = [];
+      querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+          console.log(doc.id, " => ", doc.data());
+          queryResult.push(
+            doc.id
+          )
+      });
+      setPostQueueArray(queryResult)
+      console.log(postQueueArray);
+    }});
+
+  if (user) {
+    return (
+      <>
+        <GroupFind></GroupFind>
+        <FriendFind></FriendFind>
+        <div><MakePost/></div>
+        {/* <Grid container spacing={1}>
+          {postQueueArray}
+        </Grid> */}
+        <div>
+          {postQueueArray.map((thePostID) => {
+            return <Post postID={thePostID} />
+          })}
+        </div>
+        {/* {console.log({postQueueArray})} */}
+      </>
+    )
+  } else {
+      return (
+        <>
+        <GroupFind></GroupFind>
+        <FriendFind></FriendFind>
+        <Banner/>
+        <Description/>
+        </>
+    
+      )
 }
+}
+
 
 export default Homepage
